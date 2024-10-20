9da8312cfb0e4d4d52093f73b2d8ae234d3c28ff
Jake Stewart <jake.stephen.stewart@gmail.com>
2024-10-20 21:48:00 +0800

    update

--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -3,13 +3,12 @@
 previewText() {
     # clp "$1"
     # bat --color=always --style=plain --theme=ansi "$1"
-    bat --color always --theme custom --style plain "$1"
+    bat --color always --theme custom --style plain --paging never "$1"
     # ccat --color=always "$1"
 }
 
-case $(file --mime-type -Lb "$f") in
-    text/* \
-    | application/json)
+case $(file --mime-type -Lb "$1") in
+    text/* | application/json)
         previewText "$1"
         ;;
 esac
